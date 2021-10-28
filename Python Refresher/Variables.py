d9f56957b8cca9dfa1a152d954034ef0c788eb00
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2021-10-28 17:47:31 +0530

    initiated

---
+++ Python Refresher/Variables.py
@@ -0,0 +1,19 @@
+a = 123 #int
+b = 1.23 #float
+c = "faraz" #str
+
+# Types
+print(type(a),type(b),type(c))
+
+# string addition and multiplication
+print((c + ' ') * 5)
+
+# assignment operation
+
+var1 = 123
+
+var2 = var1
+
+var1 = 'faraz'
+
+print("{} {}".format(var1,var2))
